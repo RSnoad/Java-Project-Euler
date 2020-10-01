e50f958941c7db916d6caa286b3d79cdb000864c
Richard Snoad <r.snoad@elsevier.com>
2020-10-01 16:57:40 +0100

    Problem 4 solved in Java

---
+++ src/Problem_4.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+// Find largest number that is a factor of 3 digit numbers.
+
+public class Problem_4 {
+    public static void main(String[] args) {
+
+        List<Integer> palindromeList = new ArrayList<>();
+
+        for (int i = 100; i <= 1000; i++) {
+            for (int j = 100; j <= 1000; j++) {
+                int k = i * j;
+
+
+                if (palindrome(k) == true) {
+                    palindromeList.add(k);
+                }
+            }
+        }
+
+        System.out.println(Collections.max(palindromeList));
+
+    }
+// Method to check if a number is a palindrome.
+    public static boolean palindrome(int n) {
+       String reverse =  new StringBuilder(Integer.toString(n)).reverse().toString();
+//        System.out.println(reverse);
+//        boolean bool = reverse.equals(Integer.toString(n));
+//        System.out.println(bool);
+       return reverse.equals(Integer.toString(n));
+    }
+}
+
+
+
